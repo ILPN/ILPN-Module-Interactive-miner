a521cb410a203a32e754b202c058412842a680a7
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-10-11 15:09:47 +0200

    cumulative buttons styles

--- projects/module/src/app/cumulative-slider/cumulative-slider.component.ts
+++ projects/module/src/app/cumulative-slider/cumulative-slider.component.ts
@@ -1,9 +1,17 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnDestroy} from '@angular/core';
 import {PartialOrderNetWithContainedTraces} from 'ilpn-components';
+import {FormControl} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
+enum ButtonState {
+    DESELECTED,
+    SELECTED,
+    SUGGESTED
+}
 
 interface ButtonConfig {
     cumulated: number;
+    state: ButtonState
 }
 
 @Component({
@@ -11,14 +19,22 @@ interface ButtonConfig {
     templateUrl: './cumulative-slider.component.html',
     styleUrls: ['./cumulative-slider.component.scss']
 })
-export class CumulativeSliderComponent {
+export class CumulativeSliderComponent implements OnDestroy {
 
+    private _sub: Subscription;
     private _pos: Array<PartialOrderNetWithContainedTraces> = [];
 
     public buttons: Array<ButtonConfig> = [];
     public total = 0;
+    public fc: FormControl;
 
     constructor() {
+        this.fc = new FormControl(0);
+        this._sub = this.fc.valueChanges.subscribe(() => this.processSliderChange());
+    }
+
+    ngOnDestroy(): void {
+        this._sub.unsubscribe();
     }
 
     @Input()
@@ -26,16 +42,24 @@ export class CumulativeSliderComponent {
         this._pos = pos;
         this.total = this._pos.reduce((acc, po) => acc + po.net.frequency!, 0);
         this.generateButtonConfig();
+        this.fc.setValue(0);
     }
 
     private generateButtonConfig() {
         let runningTotal = 0;
         this.buttons = this._pos.map(po => {
             const c = {
-                cumulated: runningTotal + po.net.frequency!
+                cumulated: runningTotal + po.net.frequency!,
+                state: ButtonState.DESELECTED,
             };
             runningTotal += po.net.frequency!;
             return c;
         });
+    }
+
+    private processSliderChange() {
+        for (let i = 0; i < this.buttons.length; i++) {
+            this.buttons[i].state = i <= this.fc.value ? ButtonState.SELECTED : ButtonState.DESELECTED;
+        }
     }
 }
