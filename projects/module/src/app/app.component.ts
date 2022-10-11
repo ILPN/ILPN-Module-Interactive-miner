f508efcc5bcde9a2e996890dc5e6651153edc9cb
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-10-11 13:00:05 +0200

    simple layout

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,9 +1,21 @@
 import {Component} from '@angular/core';
+import {DropFile, FD_LOG, Trace, XesLogParserService} from 'ilpn-components';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+
+    fdLog = FD_LOG;
+
+    log: Array<Trace> | undefined;
+
+    constructor(private _logParser: XesLogParserService) {
+    }
+
+    processUpload(files: Array<DropFile>) {
+        this.log = this._logParser.parse(files[0].content);
+    }
 }
